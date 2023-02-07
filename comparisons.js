27a032d316dcb7054d967ec849523a9bc899d12d
Laura Guerra <l.garciaguerra1@gmail.com>
2023-02-07 06:22:34 -0500

    Add answers to comparisons part three

--- comparisons.js
+++ comparisons.js
@@ -100,15 +100,24 @@ var age = 1;
 
 // YOU DO:
 // Determine if the dog loves to play and loves treats
+console.log(lovesToPlay && lovesTreats);
 
 
 // Determine if the dog loves to play and loves the dog park
+console.log(lovesToPlay && lovesDogPark);
 
 
 // Determine if the dog loves to play or loves the dog park
+console.log(lovesToPlay || lovesDogPark);
 
 
 // Determine if the dog loves to play and is a puppy
+console.log(lovesToPlay && age <= 1);
 
 // What did your final line of code evaluate to? Why do you think that is? Explain.
-// ANSWER:
+// ANSWER: I used the condition, age is less than or equal to one, to represent whether the dog is a puppy. 
+// My final line evaluates to true because the dog meets both the requirements of  my comparison. 
+// The dog loves to play and the age is less than or equal to one.
+// Another solution (listed below) would be to declare a new variable, isPuppy, and use that instead of age <= 1. 
+var isPuppy = true; 
+console.log(lovesToPlay && isPuppy); 

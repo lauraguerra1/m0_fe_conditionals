d81769e4e5d38ca5b866d1d6db803f96ea3b8bcd
Laura Guerra <l.garciaguerra1@gmail.com>
2023-02-07 09:18:16 -0500

    Add missing semicolons

--- codeChallenges/spicy.js
+++ codeChallenges/spicy.js
@@ -1,12 +1,12 @@
 var number = 16; 
 
 if (number % 3 === 0 && number % 5 === 0) {
-    console.log("FizzBuzz")
+    console.log("FizzBuzz");
 } else if (number % 3 === 0){
     console.log("Fizz");
 } else if (number % 5 === 0) {
     console.log("Buzz");
 } else {
-    console.log(`${number}`)
+    console.log(`${number}`);
 }
 

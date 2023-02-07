6d7ed4dd5d37e5932011c796e0b36af546c78ac7
Laura Guerra <l.garciaguerra1@gmail.com>
2023-02-07 12:21:02 -0500

    Fix a typo

--- codeChallenges/spicy.js
+++ codeChallenges/spicy.js
@@ -2,7 +2,7 @@ var number = 16;
 
 if (number % 3 === 0 && number % 5 === 0) {
     console.log("FizzBuzz");
-} else if (number % 3 === 0){
+} else if (number % 3 === 0) {
     console.log("Fizz");
 } else if (number % 5 === 0) {
     console.log("Buzz");

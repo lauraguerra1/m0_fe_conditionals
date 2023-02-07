645742d0f52289f769ab0085cb84fc059e748f7c
Laura Guerra <l.garciaguerra1@gmail.com>
2023-02-07 09:17:21 -0500

    Create a program that checks for multiples of 3 and 5

---
+++ codeChallenges/spicy.js
@@ -0,0 +1,12 @@
+var number = 16; 
+
+if (number % 3 === 0 && number % 5 === 0) {
+    console.log("FizzBuzz")
+} else if (number % 3 === 0){
+    console.log("Fizz");
+} else if (number % 5 === 0) {
+    console.log("Buzz");
+} else {
+    console.log(`${number}`)
+}
+

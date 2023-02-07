8855062c746088fa222ef30ba800a7fd8362bd85
Laura Guerra <l.garciaguerra1@gmail.com>
2023-02-07 09:00:30 -0500

    Create a program that evaluates car rental viability

---
+++ codeChallenges/medium.js
@@ -0,0 +1,10 @@
+var goodDrivingRecord = true; 
+var age = 24; 
+
+if (goodDrivingRecord === true && age >= 25) {
+    console.log("You will receive a discount on the rental car")
+} else if (goodDrivingRecord === true || age >= 25){
+    console.log("You will pay full price for the rental car")
+} else if (goodDrivingRecord === false && age < 25) {
+    console.log("You will need someone else to sign for the rental car")
+}
